a36c34998705f4a9ad4cd945c449c5860a681653
mahi25soni <msdraj02@protonmail.com>
2024-05-04 21:06:52 +0530

    get user done

--- server.js
+++ server.js
@@ -2,14 +2,19 @@ const express = require("express")
 const cookieParser = require("cookie-parser")
 require("dotenv").config();
 
+const authRoutes = require("./routes/auth.routes.js");
+const messageRoutes = require("./routes/message.routes.js");
+const userRoutes = require("./routes/user.routes.js");
+
+
 
 const app = express();
-
-
 app.use(express.json())
 app.use(cookieParser())
 
-app.use("/api/auth", );
+app.use("/api/auth", authRoutes);	
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 
 const PORT = process.env.PORT
